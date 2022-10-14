b294ea229749aee9ce1fe4da7bda2c8133466887
shivamj291 <shivamj291@gmail.com>
2022-10-14 23:06:34 +0530

    signup

---
+++ signup page/login.js
@@ -0,0 +1,18 @@
+document.querySelector("#btn").addEventListener("click",call);
+var person=JSON.parse(localStorage.getItem("array"));
+function call(){
+   event.preventDefault();
+    var mail=document.getElementById("mail").value;
+    var password=document.getElementById("password").value;
+
+   let element=person.find(item => item.mail===mail&& item.password===password );
+   
+   if(element===undefined){
+      document.getElementById("ull").style.display="contents";
+      document.getElementById("adjust").style.display="contents";
+      document.getElementById("logp").style.display="contents";
+      document.getElementById("logimage").style.display="contents";
+   }else{
+    alert("succesfull");
+   }
+}
